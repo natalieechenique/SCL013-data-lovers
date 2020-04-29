d68bf50febed6953a89d90b0c4ab0e6c204f07f4
nataliee@github.com <natalie.terapias@gmail.com>
2020-04-29 12:29:29 -0400

    boton inicio

--- src/main.js
+++ src/main.js
@@ -1,6 +1,52 @@
-import { example } from './data.js';
+
+
+
+//import { example } from './data.js';//
+
+
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+
+import dataPotter from './data/potter/potter.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(dataPotter);
+
+
+
+
+
+
+
+
+const ocultarYMostrar = (ocultarID, mostrarID) => {
+    document.getElementById(ocultarID).classList.add("oculto");
+    document.getElementById(mostrarID).classList.remove("oculto");
+}
+
+
+const selectIngresar = () => {
+   ocultarYMostrar("inicio","clasificaciones");
+}
+
+const buttonSelection = document.getElementById("selectIngresar");
+  document.addEventListener("click",selectIngresar);
+
+
+
+  const selectVolver=()=> {
+    ocultarYMostrar("clasificaciones","inicio");
+   }
+  
+  const buttonCancel=document.getElementById("selectVolver");
+  buttonCancel.addEventListener("click",selectVolver);
+
+//  const selectVolver = () => {
+ //   ocultarYMostrar("clasificaciones","inicio");
+ //}
+ 
+ //const buttonVolver = document.getElementById("selectVolver");
+ //  document.addEventListener("click",selectVolver);//
+ 
+
+
+
