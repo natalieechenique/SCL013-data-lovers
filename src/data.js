90c6f091f7f114f3f127b9637b7c3116a120da0a
Carlareneedc <carla.renee.diazc@gmail.com>
2020-05-12 10:35:53 -0400

    cambios nuevos

--- src/data.js
+++ src/data.js
@@ -1,6 +1,9 @@
-//En este lugar estamos exportando desde potter.js, las funciones que vamos creando acá, tomando desde la data general enviada por Laboratoria,
+
+// En este lugar estamos exportando desde potter.js, las funciones que vamos creando acá, tomando desde la data general enviada por Laboratoria,
 // para ser utilizadas en main.js donde éste trabajara con DOM para ver en HTML toda nuestra página con las funciones
 //<------- La data general ya está creada por Laboratoria y ya la llamamos en js, con el nombre dataPotter.
+
+
 //<---Funcion filtrar por casas para enviar (exportar) a main.js
 // Dentro de la func filterHouse, creamos la variable let characterHouse con su función dentro,
 //que mostrara los personajes de cada casa ya filtrados
@@ -16,10 +19,32 @@ export function filterhogwartsStudent(dataPotter) {
   let characterHouse = dataPotter.filter(character => character.hogwartsStudent === true);
   return characterHouse;
 }
-// export function filterData(data, attribute, condition) {
-//   let results = data;
-//   if (attribute && condition) {
-//     results = data.filter(item => item[attribute] === condition);
+
+
+export function filterGender(dataPotter, condition) {
+let characterHouse = dataPotter.filter(character => character.gender === condition);
+return characterHouse;
+}
+
+
+
+
+
+// export const orderData = (dataArray, condition) => {
+//   let filterOrder = dataArray; //Arreglo nuevo donde guardaremos persojes ordenados
+//   if(condition==0){
+//     return filterOrder;
 //   }
-//   return results;
+//   if(condition == 1){
+//     filterOrder.sort((a, b) => a.name.localeCompare(b.name));
+//     return filterOrder;
+//   }
+//   if(condition==2){
+//     filterOrder.sort((a, b) => a.name.localeCompare(b.name));
+//     const filterOrderReverse=filterOrder.reverse();
+//     return filterOrderReverse;
+//   }
+
 // }
+// export default orderData;
+
